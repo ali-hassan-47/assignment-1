5a48f8b08889b059556097b532683331f63ce91e
alihagd <alihgad2@gmail.com>
2026-05-11 22:52:05 +0300

    Revert "rev"
    This reverts commit 0bf2c133ccb9d8cd93f21281939caa3ec2c3efc1.

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 let x = "ali"
 let y = 5
+let y = 5
 
 
-
-console.log(x)
-console.log(y)
-console.log(z)
-console.log(x)
-console.log(y)
-console.log(z)
-console.log(x)
-console.log(y)
-console.log(z)
 console.log(x)
 console.log(y)
 console.log(z)
